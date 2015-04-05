b00c941e538fa43ca718972e671d3a93278405ec
Manasvi <msaxena2@illinois.edu>
2015-04-04 19:40:54 -0500

    more changes with working makefile

---
+++ memory_manager/test/test_alloc.c
@@ -0,0 +1,15 @@
+/*
+ * test_alloc.c
+ *
+ *  Created on: Apr 4, 2015
+ *      Author: manasvi
+ */
+
+
+#include <stdio.h>
+#include "../pool_memory_manager.h"
+
+int main()
+{
+	compressed_alloc(20);
+}
