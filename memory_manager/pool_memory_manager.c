b780ca3918984ad906eabc7b4170275135cd1848
Manasvi <msaxena2@illinois.edu>
2015-04-04 22:34:48 -0500

    fixed some compiler errors

--- memory_manager/pool_memory_manager.c
+++ memory_manager/pool_memory_manager.c
@@ -6,6 +6,7 @@
  */
 
 #include "pool_memory_manager.h"
+#include <stdlib.h>
 /* Block size is 8 MB */
 #define BLOCK_SIZE 8000000
 
@@ -13,7 +14,7 @@
  * Struct for the dictionary containing the list of blocks
  */
 
-typedef struct main_block_list {
+struct main_block_list {
 	/* pointer to the main block*/
 	void * block_ptr;
 	/* list node */
@@ -25,16 +26,29 @@ typedef struct main_block_list {
  */
 struct main_block_list * block_list_ptr = NULL;
 
+void * first_fit_add(void * block_ptr, size_t size) {
+	return NULL;
+}
+
 void * compressed_alloc(size_t size) {
 	/* case of first alloc */
 	if (!block_list_ptr) {
 		block_list_ptr = (struct main_block_list *) malloc(
 				sizeof(struct main_block_list));
 		block_list_ptr->block_ptr = malloc(sizeof(BLOCK_SIZE));
-		INIT_LIST_HEAD(&block_list_ptr->node);
+		INIT_LIST_HEAD(&(block_list_ptr->node));
 	}
 
+	/* First fit strategy for the blocks */
+	struct list_head * pos;
+	struct main_block_list * curr_node;
+	list_for_each(pos, &(block_list_ptr->node)){
+		curr_node = list_entry(pos, struct main_block_list, node);
+		void * ret_prt = first_fit_add(curr_node, size);
+	}
+	return NULL;
 }
+
 
 void free(void * ptr) {
 	printf("%s \n", "free");
