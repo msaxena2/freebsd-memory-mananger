ce2c1c0a5dacb7256497ebdfd76b8c22c4709455
Manasvi <msaxena2@illinois.edu>
2015-04-04 22:13:15 -0500

    updates to the main file :

--- memory_manager/pool_memory_manager.c
+++ memory_manager/pool_memory_manager.c
@@ -6,27 +6,37 @@
  */
 
 #include "pool_memory_manager.h"
+/* Block size is 8 MB */
+#define BLOCK_SIZE 8000000
 
 /*
  * Struct for the dictionary containing the list of blocks
  */
 
-struct main_block_list {
+typedef struct main_block_list {
 	/* pointer to the main block*/
 	void * block_ptr;
 	/* list node */
-	struct list_head * node;
+	struct list_head node;
 };
 
-void * compressed_alloc(size_t size)
-{
-	printf("%s \n", "alloc");
-	return NULL;
+/*
+ * List containing the block pointers
+ */
+struct main_block_list * block_list_ptr = NULL;
+
+void * compressed_alloc(size_t size) {
+	/* case of first alloc */
+	if (!block_list_ptr) {
+		block_list_ptr = (struct main_block_list *) malloc(
+				sizeof(struct main_block_list));
+		block_list_ptr->block_ptr = malloc(sizeof(BLOCK_SIZE));
+		INIT_LIST_HEAD(&block_list_ptr->node);
+	}
+
 }
 
-
-void free(void * ptr)
-{
+void free(void * ptr) {
 	printf("%s \n", "free");
 }
 
